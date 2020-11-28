2c2dfbf1609dfbead4442ca7782347bbbe23d62d
sdereniowski@gmail.com <sdereniowski@gmail.com>
2020-11-27 22:23:59 -0800

    changing upload logic

--- upload.php
+++ upload.php
@@ -37,12 +37,11 @@ if (isset($_POST['submit'])){
 						'Bucket' => 'elasticbeanstalk-us-west-2-722883947022',
 						'Key'    => $fileNameNew,
 						'Body'   => 'this is the body!',
-						//'SourceFile' => 'c:\samplefile.png' -- use this if you want to upload a file from a local location
+						'SourceFile' => $fileName // use this if you want to upload a file from a local location
 					]);
 					
 					// Print the body of the result by indexing into the result object.
 					echo($result);              
-					header("Location: index.php?uploadsuccess");
 				//Error if file is too big
 				} else {
 					echo "The file is too big";
