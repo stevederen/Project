12db55ec15daf40e5ca3bdfbaccf389727a2cf45
sdereniowski@gmail.com <sdereniowski@gmail.com>
2020-11-27 23:25:49 -0800

    improving logs

--- upload.php
+++ upload.php
@@ -15,9 +15,6 @@ if (isset($_POST['submit'])){
 		$fileError = $_FILES['file']['error'];
 		$fileType = $_FILES['file']['type'];
 
-		
-		echo $fileName;
-
 
 		$fileExt = explode('.', $fileName);
 		$fileActualExt = strtolower(end($fileExt));
@@ -44,6 +41,7 @@ if (isset($_POST['submit'])){
 					]);
 					
 					// Print the body of the result by indexing into the result object.
+					echo('Filename '.$fileTmpName.' uploaded as: '.$fileNameNew.'\n');      
 					echo($result);              
 				//Error if file is too big
 				} else {
