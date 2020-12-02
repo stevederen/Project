c8f9475d4c522ef4bf78993c9b237bd725af2e76
stevederen <75148369+stevederen@users.noreply.github.com>
2020-12-02 14:45:07 -0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 <form action= "upload.php" method="POST" enctype="multipart/form-data">
     <input type="file" name="file">
-    <input type="image" src="pictures/diveball.png">
+    <input type="image" src= "url(https://www.clipartmax.com/png/middle/190-1909431_dive-ball-by-neo-chu-dive-ball-pokemon.png)">
     <button type="submit" name="submit"> Upload File </button>
 </form>
 
@@ -14,4 +14,3 @@
 
 </body>
 </html>
-
