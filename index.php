6be3a695719e567c11a488e1d0bc14f848dcdfd7
stevederen <75148369+stevederen@users.noreply.github.com>
2020-12-02 13:30:59 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <button type="submit" name="submit"> Upload File </button>
 </form>
 
-<body style = "background: url(https://i.pinimg.com/originals/1c/a6/e1/1ca6e1c35e2d884230242cc0326dbf28.jpg); 
-                background-size: 100% 100%;">
+<body style = "background: url(https://i.pinimg.com/originals/1c/a6/e1/1ca6e1c35e2d884230242cc0326dbf28.jpg)">
 
 </body>
 </html>
+
