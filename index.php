12bf88452179e8efeec38976374b876d9f09e115
stevederen <75148369+stevederen@users.noreply.github.com>
2020-12-10 12:44:33 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
 <html>
 <head>
     <title>Upload Image Project</title>
-
-    <link rel="stylesheet" href="style.css">
 </head>
 
+<div id="content" align="center">
 <body>
-
 <form action= "upload.php" method="POST" enctype="multipart/form-data">
     <input type="file" name="file">
     <button type="submit" name="submit"> Upload File </button>   
@@ -19,6 +17,7 @@ Search :
 <input type="submit" value="search">
 
 </form>
+</div>
 
 <body style = "background: url(https://i.pinimg.com/originals/1c/a6/e1/1ca6e1c35e2d884230242cc0326dbf28.jpg)">
 
