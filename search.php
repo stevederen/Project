00a7fe7e94a626625ce2f2b5dfad7f9d3954fa1c
stevederen <75148369+stevederen@users.noreply.github.com>
2020-12-10 13:38:44 -0800

    Update search.php

--- search.php
+++ search.php
@@ -1,12 +1,10 @@
 <?php
 require 'vendor/autoload.php';
+use Aws\S3\S3Client;
 
 $objects = $s3->getIterator('ListObjects', [ 
     'Bucket' => $config['s3']['bucket'] ]);
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,10 +20,15 @@ $objects = $s3->getIterator('ListObjects', [
                     <th>Download</th>
                 </tr>
             </thead>
+
+            <!--Table for listing objects-->
             <tbody>
+                <!--Finds objects in bucket-->
                 <?php foreach($object as $object): ?>
                 <tr>
+                     <!--Display name of objects-->
                     <td><?php echo $object['Key']; ?></td>
+                     <!--Displays object download link-->
                     <td><a href="<?php echo $s3->getObjectUrl($config['s3']['bucket'], $object['Key']); ?>" download="<?php $object['Key']; ?>">Download</a></td>
                 </tr>
                 <?php endforeach; ?>
