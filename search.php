57bbed4e2ae37408d82791b41e2d31eab3a37695
stevederen <75148369+stevederen@users.noreply.github.com>
2020-12-10 19:18:02 -0800

    Update search.php

--- search.php
+++ search.php
@@ -10,10 +10,11 @@ var_dump($s3);
 //Creates objects variable, links it to objects in bucket
 $objects = $s3->getIterator('ListObjects', array(
     'Bucket' =>'elasticbeanstalk-us-west-2-722883947022',
-    'Prefix' => '/files/txt/'
+    'Prefix' => 'files/txt/'
 ));
 var_dump($s3->getIterator('ListObjects', array(
-    'Bucket' =>'elasticbeanstalk-us-west-2-722883947022'
+    'Bucket' =>'elasticbeanstalk-us-west-2-722883947022',
+    'Prefix' => 'files/'
 )));
 ?>
 
