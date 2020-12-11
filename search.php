e72beff48c545342a938d1da236894591fcc5ad4
stevederen <75148369+stevederen@users.noreply.github.com>
2020-12-10 17:59:11 -0800

    Update search.php

--- search.php
+++ search.php
@@ -1,11 +1,3 @@
-<?php
-require 'vendor/autoload.php';
-use Aws\S3\S3Client;
-
-//Creates objects variable, links it to objects in bucket
-$objects = $s3->getIterator('ListObjects', ['Bucket' => $config['s3']['bucket'] ]);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -18,21 +10,8 @@ $objects = $s3->getIterator('ListObjects', ['Bucket' => $config['s3']['bucket'] 
                 <tr>
                     <th>File</th>
                     <th>Download</th>
-                </tr>
-            </thead>
-
-            <!--Table for listing objects-->
-            <tbody>
-                <!--Finds objects in bucket-->
-                <?php foreach($objects as $object): ?>
-                <tr>
-                     <!--Display name of objects-->
-                    <td><?php echo $object['Key']; ?></td>
-                     <!--Displays object download link-->
-                    <td><a href="<?php echo $s3->getObjectUrl($config['s3']['bucket'], $object['Key']); ?>" download="<?php $object['Key']; ?>">Download</a></td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
+      
+          
         </table>
     </body>
 </html>
