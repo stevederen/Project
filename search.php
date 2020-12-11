f7919ebc664672d55fb70b19687c28a701e4469a
stevederen <75148369+stevederen@users.noreply.github.com>
2020-12-10 18:11:10 -0800

    Update search.php

--- search.php
+++ search.php
@@ -1,3 +1,11 @@
+<?php
+require 'vendor/autoload.php';
+use Aws\S3\S3Client;
+
+//Creates objects variable, links it to objects in bucket
+$objects = $s3->getIterator('ListObjects', ['elasticbeanstalk-us-west-2-722883947022' => $config['s3']['bucket'] ]);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -10,8 +18,19 @@
                 <tr>
                     <th>File</th>
                     <th>Download</th>
-      
-          
+                </tr>
+            </thead>
+
+            <!--Table for listing objects-->
+            <tbody>
+                <!--Finds objects in bucket-->
+                <?php foreach($objects as $object): ?>
+                <tr>
+                     <!--Display name of objects-->
+                    <td><?php echo $object['Key']; ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
         </table>
     </body>
 </html>
